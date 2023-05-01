d7c2f799b6f064b6d6e35ee929a3e63a4aec0203
HeDevedUp <uzomaesse@gmail.com>
2023-05-01 19:01:09 +0100

    feat : added add button

--- src/layouts/Programmes/index.js
+++ src/layouts/Programmes/index.js
@@ -1,5 +1,6 @@
 // @mui material components
 import Card from "@mui/material/Card";
+import Grid from "@mui/material/Grid";
 
 // Soft UI Dashboard React components
 import SoftBox from "components/SoftBox";
@@ -15,6 +16,15 @@ import Table from "examples/Tables/Table";
 import authorsTableData from "layouts/Programmes/data/authorsTableData";
 import projectsTableData from "layouts/Programmes/data/projectsTableData";
 
+import DefaultProjectCard from "examples/Cards/ProjectCards/DefaultProjectCard";
+import PlaceholderCard from "examples/Cards/PlaceholderCard";
+import homeDecor1 from "assets/images/home-decor-1.jpg";
+import homeDecor2 from "assets/images/home-decor-2.jpg";
+import homeDecor3 from "assets/images/home-decor-3.jpg";
+import team1 from "assets/images/team-1.jpg";
+import team2 from "assets/images/team-2.jpg";
+import team3 from "assets/images/team-3.jpg";
+import team4 from "assets/images/team-4.jpg";
 function Programmes() {
   const { columns, rows } = authorsTableData;
   const { columns: prCols, rows: prRows } = projectsTableData;
@@ -22,6 +32,94 @@ function Programmes() {
   return (
     <DashboardLayout>
       <DashboardNavbar />
+
+      <SoftBox mb={3}>
+        <Card>
+          <SoftBox pt={2} px={2}>
+            <SoftBox mb={0.5}>
+              <SoftTypography variant="h6" fontWeight="medium">
+                Projects
+              </SoftTypography>
+            </SoftBox>
+            <SoftBox mb={1}>
+              <SoftTypography variant="button" fontWeight="regular" color="text">
+                Architects design houses
+              </SoftTypography>
+            </SoftBox>
+          </SoftBox>
+          <SoftBox p={2}>
+            <Grid container spacing={3}>
+              <Grid item xs={12} md={6} xl={3}>
+                <DefaultProjectCard
+                  image={homeDecor1}
+                  label="project #2"
+                  title="modern"
+                  description="As Uber works through a huge amount of internal management turmoil."
+                  action={{
+                    type: "internal",
+                    route: "/pages/profile/profile-overview",
+                    color: "info",
+                    label: "view project",
+                  }}
+                  authors={[
+                    { image: team1, name: "Elena Morison" },
+                    { image: team2, name: "Ryan Milly" },
+                    { image: team3, name: "Nick Daniel" },
+                    { image: team4, name: "Peterson" },
+                  ]}
+                />
+              </Grid>
+              <Grid item xs={12} md={6} xl={3}>
+                <DefaultProjectCard
+                  image={homeDecor2}
+                  label="project #1"
+                  title="scandinavian"
+                  description="Music is something that every person has his or her own specific opinion about."
+                  action={{
+                    type: "internal",
+                    route: "/pages/profile/profile-overview",
+                    color: "info",
+                    label: "view project",
+                  }}
+                  authors={[
+                    { image: team3, name: "Nick Daniel" },
+                    { image: team4, name: "Peterson" },
+                    { image: team1, name: "Elena Morison" },
+                    { image: team2, name: "Ryan Milly" },
+                  ]}
+                />
+              </Grid>
+              <Grid item xs={12} md={6} xl={3}>
+                <DefaultProjectCard
+                  image={homeDecor3}
+                  label="project #3"
+                  title="minimalist"
+                  description="Different people have different taste, and various types of music."
+                  action={{
+                    type: "internal",
+                    route: "/pages/profile/profile-overview",
+                    color: "info",
+                    label: "view project",
+                  }}
+                  authors={[
+                    { image: team4, name: "Peterson" },
+                    { image: team3, name: "Nick Daniel" },
+                    { image: team2, name: "Ryan Milly" },
+                    { image: team1, name: "Elena Morison" },
+                  ]}
+                />
+              </Grid>
+              <Grid item xs={12} md={6} xl={3}>
+                <PlaceholderCard title={{ variant: "h5", text: "New project" }} outlined />
+              </Grid>
+            </Grid>
+          </SoftBox>
+        </Card>
+      </SoftBox>
+
+
+
+      
       <SoftBox py={3}>
         <SoftBox mb={3}>
           <Card>
--- src/layouts/Tokens/index.js
+++ src/layouts/Tokens/index.js
@@ -1,6 +1,6 @@
 // @mui material components
 import Card from "@mui/material/Card";
-
+import Grid from "@mui/material/Grid";
 // Soft UI Dashboard React components
 import SoftBox from "components/SoftBox";
 import SoftTypography from "components/SoftTypography";
@@ -11,9 +11,20 @@ import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
 import Table from "examples/Tables/Table";
 
+import homeDecor1 from "assets/images/home-decor-1.jpg";
+import homeDecor2 from "assets/images/home-decor-2.jpg";
+import homeDecor3 from "assets/images/home-decor-3.jpg";
+import team1 from "assets/images/team-1.jpg";
+import team2 from "assets/images/team-2.jpg";
+import team3 from "assets/images/team-3.jpg";
+import team4 from "assets/images/team-4.jpg";
+
 // Data
 import authorsTableData from "layouts/Tokens/data/authorsTableData";
 import projectsTableData from "layouts/Tokens/data/projectsTableData";
+import DefaultProjectCard from "examples/Cards/ProjectCards/DefaultProjectCard";
+import PlaceholderCard from "examples/Cards/PlaceholderCard";
+
 
 function Tokens() {
   const { columns, rows } = authorsTableData;
@@ -22,6 +33,94 @@ function Tokens() {
   return (
     <DashboardLayout>
       <DashboardNavbar />
+      
+      <SoftBox mb={3}>
+        <Card>
+          <SoftBox pt={2} px={2}>
+            <SoftBox mb={0.5}>
+              <SoftTypography variant="h6" fontWeight="medium">
+                Projects
+              </SoftTypography>
+            </SoftBox>
+            <SoftBox mb={1}>
+              <SoftTypography variant="button" fontWeight="regular" color="text">
+                Architects design houses
+              </SoftTypography>
+            </SoftBox>
+          </SoftBox>
+          <SoftBox p={2}>
+            <Grid container spacing={3}>
+              <Grid item xs={12} md={6} xl={3}>
+                <DefaultProjectCard
+                  image={homeDecor1}
+                  label="project #2"
+                  title="modern"
+                  description="As Uber works through a huge amount of internal management turmoil."
+                  action={{
+                    type: "internal",
+                    route: "/pages/profile/profile-overview",
+                    color: "info",
+                    label: "view project",
+                  }}
+                  authors={[
+                    { image: team1, name: "Elena Morison" },
+                    { image: team2, name: "Ryan Milly" },
+                    { image: team3, name: "Nick Daniel" },
+                    { image: team4, name: "Peterson" },
+                  ]}
+                />
+              </Grid>
+              <Grid item xs={12} md={6} xl={3}>
+                <DefaultProjectCard
+                  image={homeDecor2}
+                  label="project #1"
+                  title="scandinavian"
+                  description="Music is something that every person has his or her own specific opinion about."
+                  action={{
+                    type: "internal",
+                    route: "/pages/profile/profile-overview",
+                    color: "info",
+                    label: "view project",
+                  }}
+                  authors={[
+                    { image: team3, name: "Nick Daniel" },
+                    { image: team4, name: "Peterson" },
+                    { image: team1, name: "Elena Morison" },
+                    { image: team2, name: "Ryan Milly" },
+                  ]}
+                />
+              </Grid>
+              <Grid item xs={12} md={6} xl={3}>
+                <DefaultProjectCard
+                  image={homeDecor3}
+                  label="project #3"
+                  title="minimalist"
+                  description="Different people have different taste, and various types of music."
+                  action={{
+                    type: "internal",
+                    route: "/pages/profile/profile-overview",
+                    color: "info",
+                    label: "view project",
+                  }}
+                  authors={[
+                    { image: team4, name: "Peterson" },
+                    { image: team3, name: "Nick Daniel" },
+                    { image: team2, name: "Ryan Milly" },
+                    { image: team1, name: "Elena Morison" },
+                  ]}
+                />
+              </Grid>
+              <Grid item xs={12} md={6} xl={3}>
+                <PlaceholderCard title={{ variant: "h5", text: "New project" }} outlined />
+              </Grid>
+            </Grid>
+          </SoftBox>
+        </Card>
+      </SoftBox>
+
+
+
+
       <SoftBox py={3}>
         <SoftBox mb={3}>
           <Card>
