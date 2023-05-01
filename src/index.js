bcfed88aae17629f912dbbd8f77533b05193d2fa
HeDevedUp <uzomaesse@gmail.com>
2023-05-01 14:33:49 +0100

    ggs

---
+++ src/index.js
@@ -0,0 +1,17 @@
+
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "App";
+
+import { SoftUIControllerProvider } from "context";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <SoftUIControllerProvider>
+      <App />
+    </SoftUIControllerProvider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
